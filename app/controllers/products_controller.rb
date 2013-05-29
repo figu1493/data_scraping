4fd2e44562dee386b56f0f8e5a95df30a99ab63b
John Figueiredo <jfigueir33@gmail.com>
2013-05-29 14:18:15 -0700

    Changed #destroy controller with redirect

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -52,6 +52,7 @@ class ProductsController < ApplicationController
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
+    redirect_to products_url
   end
 
   def destroy_all
